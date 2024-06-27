a360b8d68c89d929e8736f6c1f5a966d810e27ca
Daniel M <70288574+danyelliot@users.noreply.github.com>
2024-06-26 22:38:09 -0500

    update multiple targets and models

--- static/main.js
+++ static/main.js
@@ -1,35 +1,37 @@
-import {loadGLTF} from "./libs/loader.js";
-import {mockWithVideo} from './libs/camera-mock.js';
+import { loadGLTF } from "./libs/loader.js";
+import { mockWithVideo } from './libs/camera-mock.js';
 const THREE = window.MINDAR.IMAGE.THREE;
 
 document.addEventListener('DOMContentLoaded', () => {
-  const start = async() => {
-    //mockWithVideo('../../assets/mock-videos/musicband1.mp4');
+  const start = async () => {
+    const imageTargets = [
+      { src: '/static/assets/targets/facultad_de_ciencias_letras.mind', model: '/static/assets/models/scientist/scene.gltf', scale: { x: 0.1, y: 0.1, z: 0.1 } },
+      { src: '/static/assets/targets/target_uni.mind', model: '/static/assets/models/model2/scene.gltf', scale: { x: 0.9, y: 0.9, z: 0.9 } }
+    ];
 
-    const mindarThree = new window.MINDAR.IMAGE.MindARThree({
-      container: document.body,
-      imageTargetSrc: '/static/assets/targets/facultad_de_ciencias_letras.mind',
-    });
-    const {renderer, scene, camera} = mindarThree;
+    for (let target of imageTargets) {
+      const mindarThree = new window.MINDAR.IMAGE.MindARThree({
+        container: document.body,
+        imageTargetSrc: target.src,
+      });
+      const { renderer, scene, camera } = mindarThree;
 
-    const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
-    scene.add(light);
-    //
-    const raccoon = await loadGLTF('/static/assets/models/scientist/scene.gltf');
-    //const raccoon = await loadGLTF('/static/assets/models/upch-dia/images (1).gltf');
-    //                      X     Y    Z
-    raccoon.scene.scale.set(0.1, 0.1, 0.1);
-    //                         X    Y   Z
-    raccoon.scene.position.set(0, 0, 0);
+      const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
+      scene.add(light);
 
+      const model = await loadGLTF(target.model);
 
-    const anchor = mindarThree.addAnchor(0);
-    anchor.group.add(raccoon.scene);
+      model.scene.scale.set(target.scale.x, target.scale.y, target.scale.z);
+      model.scene.position.set(0, 0, 0);
 
-    await mindarThree.start();
-    renderer.setAnimationLoop(() => {
-      renderer.render(scene, camera);
-    });
+      const anchor = mindarThree.addAnchor(0);
+      anchor.group.add(model.scene);
+
+      await mindarThree.start();
+      renderer.setAnimationLoop(() => {
+        renderer.render(scene, camera);
+      });
+    }
   }
   start();
 });
