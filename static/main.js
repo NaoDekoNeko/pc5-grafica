4ebe0703c984bc1813c627828fbf0ca4cbc242c6
Daniel M <70288574+danyelliot@users.noreply.github.com>
2024-06-27 19:10:06 -0500

    update models and target

--- static/main.js
+++ static/main.js
@@ -6,7 +6,12 @@ document.addEventListener('DOMContentLoaded', () => {
   const start = async () => {
     const imageTargets = [
       { src: '/static/assets/targets/facultad_de_ciencias_letras.mind', model: '/static/assets/models/scientist/scene.gltf', scale: { x: 0.1, y: 0.1, z: 0.1 } },
-      { src: '/static/assets/targets/target_uni.mind', model: '/static/assets/models/model2/scene.gltf', scale: { x: 0.9, y: 0.9, z: 0.9 } }
+      { src: '/static/assets/targets/target_uni.mind', model: '/static/assets/models/model2/scene.gltf', scale: { x: 0.9, y: 0.9, z: 0.9 }},
+      { src: '/static/assets/targets/aire_acondicionado.mind', model: '/static/assets/models/snowflake/scene.gltf', scale: { x: 0.01, y: 0.01, z: 0.01 }},
+      { src: '/static/assets/targets/centro_investigacion.mind', model: '/static/assets/models/microscope/scene.gltf', scale: { x: 0.1, y: 0.1, z: 0.1 }},
+      { src: '/static/assets/targets/biblioteca_fc', model: '/static/assets/models/book/scene.gltf', scale: { x: 0.1, y: 0.1, z: 0.1 }},
+      { src: '/static/assets/targets/ieee', model: '/static/assets/models/commodore/scene.gltf', scale: { x: 0.1, y: 0.1, z: 0.1 }}
+
     ];
 
     for (let target of imageTargets) {
